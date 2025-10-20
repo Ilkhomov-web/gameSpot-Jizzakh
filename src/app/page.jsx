7f1584ce85dc186ccbf577e962a5ac86af23f29e
ilkhomov-web <ilhomovelyor119@gmail.com>
2025-10-20 15:23:14 +0500

    some change

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 "use client";
+import { useThemeContext } from "@/context/ThemeContext";
 import { Button, Container, Typography, Card, CardContent } from "@mui/material";
 
 export default function HomePage() {
+  const {mode} = useThemeContext()
   return (
     <Container>
-      <Typography sx={{color: 'white'}}>Elka</Typography>
+      <Typography sx={{color: mode === "dark" ? "white" : "#3B0270", fontSize: {xs: '12px', lg: '30px'}}}>Premium Clublar </ Typography>
     </Container>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Button, Container, Grid, List, ListItem, Typography } from "@mui/material";
+import { Box, Button, Container, Grid, List, ListItem, Typography, useTheme } from "@mui/material";
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
 import GTranslateIcon from '@mui/icons-material/GTranslate';
@@ -9,14 +9,16 @@ import { useThemeContext } from '@/context/ThemeContext';
 
 const Navbar = () => {
   const {mode, toggleTheme} = useThemeContext()
+  const theme  = useTheme()
+  const isMobile = theme.breakpoints.down("sm")
 
   return (
     <Box sx={{ padding: '20px 0px'}}>
       <Container maxWidth="lg">
       <Grid container>
         <Grid item size={{xs: 6, sm: 6, md: 6, lg: 6, xl: 6,}} sx={{display: 'flex', alignItems: 'center', gap: '10px'}} >
-            <Box component={"img"} width={"100px"} src={"/logo.png"}></Box>
-            <Typography variant='h4' sx={{ color: mode === "dark" ? "white":  "#3B0270" }}>GameSpot Jizzakh </Typography>
+            <Box component={"img"} width={{xs: "50px", lg: "100px"}} src={"/logo.png"}></Box>
+            <Typography sx={{ color: mode === "dark" ? "white":  "#3B0270", fontSize: {xs: "12px", lg: "35px"} }}>GameSpot Jizzakh </Typography>
         </Grid>
         <Grid item size={{xs: 6, sm: 6, md: 6, lg: 6, xl: 6,}} sx={{display: 'flex', alignItems: 'center', gap: '10px', justifyContent: 'flex-end'}}>
             <List>
